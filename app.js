6c26f845da4f46cda7d2a459cdfdfd179fb2879c
NataliePineda17 <natalie.pineda@ct.uneatlantico.es>
2024-04-18 19:37:24 +0200

    the new changes are when you click on see more is going to redirect you to the websites it should

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//step 1: get DOM
+// Step 1: Get DOM
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
 
@@ -12,26 +12,43 @@ thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 let timeRunning = 3000;
 let timeAutoNext = 7000;
 
-nextDom.onclick = function(){
-    showSlider('next');    
+nextDom.onclick = function () {
+    showSlider('next');
 }
 
-prevDom.onclick = function(){
-    showSlider('prev');    
+prevDom.onclick = function () {
+    showSlider('prev');
 }
+
+// Add event listeners to "SEE MORE" buttons
+document.querySelectorAll('.buttons button').forEach((button, index) => {
+    button.addEventListener('click', function () {
+        // Define redirection URLs based on the topic index
+        const redirectionURLs = [
+            'https://kronos.ctdesarrollo-sdr.org/mi-tiempo-hoy',
+            'https://soporte.uneatlantico.es/scp/',
+            'https://servicio-deportes.uneatlantico.es/tag/reservas/',
+            'https://extensiones.uneatlantico.es/'
+        ];
+
+        // Redirect to the corresponding URL when clicking "SEE MORE"
+        window.location.href = redirectionURLs[index];
+    });
+});
+
 let runTimeOut;
 let runNextAuto = setTimeout(() => {
     next.click();
 }, timeAutoNext)
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+function showSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-    if(type === 'next'){
+
+    if (type === 'next') {
         SliderDom.appendChild(SliderItemsDom[0]);
         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
         carouselDom.classList.add('next');
-    }else{
+    } else {
         SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
         thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
         carouselDom.classList.add('prev');
